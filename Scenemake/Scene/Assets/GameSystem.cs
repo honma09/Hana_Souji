1d115dfd12ea43173c8c58d3efe7628e756c8916
KPC-017A2140\ray <ray19990317@gmail.com>
2018-11-09 10:26:52 +0900

    Unityファイル追加

---
+++ Scenemake/Scene/Assets/GameSystem.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class GameSystem : MonoBehaviour
+{
+
+    //　スタートボタンを押したら実行する
+    public void GameStart()
+    {
+        SceneManager.LoadScene("Game");
+    }
+}
